9ef75588d0257b27b0357d89ac2caa425bd55c3d
FelipePelegrino <felipe.pelegrino99@gmail.com>
2021-09-27 20:46:33 -0300

    feat: add fun getUser

--- app/src/main/java/com/gmail/devpelegrino/cronoagua/repository/UserProfileRepository.kt
+++ app/src/main/java/com/gmail/devpelegrino/cronoagua/repository/UserProfileRepository.kt
@@ -9,11 +9,20 @@ import kotlinx.coroutines.withContext
 class UserProfileRepository(private val database: UserProfileDatabase) {
 
     private lateinit var _users : LiveData<List<UserProfile>>
+    private lateinit var _user : LiveData<UserProfile>
+
 
     suspend fun getAllUsers() : LiveData<List<UserProfile>> {
         withContext(Dispatchers.IO) {
             _users = database.userProfileDao.getAllUserProfile()
         }
         return _users
+    }
+
+    suspend fun getUser(id: Int) : LiveData<UserProfile> {
+        withContext(Dispatchers.IO) {
+            _user = database.userProfileDao.getUserProfile(id)
+        }
+        return _user
     }
 }
