b54357c693f92d831a6ad49316eba59678a29a8e
FelipePelegrino <felipe.pelegrino99@gmail.com>
2021-10-13 17:23:41 -0300

    feat: alter colors

--- app/src/main/java/com/gmail/devpelegrino/cronoagua/ui/WaterManagementFragment.kt
+++ app/src/main/java/com/gmail/devpelegrino/cronoagua/ui/WaterManagementFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.gmail.devpelegrino.cronoagua.R
 
-class WaterManagerFragment : Fragment() {
+class WaterManagementFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
