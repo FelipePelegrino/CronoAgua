fb6554e507e72b48f4d6442301a63b6750a7c063
FelipePelegrino <felipe.pelegrino99@gmail.com>
2021-11-11 15:22:33 -0300

    refactor class

--- app/src/main/java/com/gmail/devpelegrino/cronoagua/notify_system/NotifyWorker.kt
+++ app/src/main/java/com/gmail/devpelegrino/cronoagua/notify_system/NotifyWorker.kt
@@ -1,23 +1,11 @@
 package com.gmail.devpelegrino.cronoagua.notify_system
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.media.RingtoneManager
-import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.gmail.devpelegrino.cronoagua.R
-import com.gmail.devpelegrino.cronoagua.ui.MainActivity
 
-class NotifyWorker(appContext: Context, workerParams: WorkerParameters)
-    : Worker(appContext, workerParams) {
+class NotifyWorker(appContext: Context, workerParams: WorkerParameters) :
+    Worker(appContext, workerParams) {
 
     private var context = appContext
 
